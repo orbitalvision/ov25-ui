7d6d6bb4ad82fe4fb1e41f48e5d3cca1f3382a5e
tom williams <tom.williams@orbital.vision>
2025-10-15 11:09:36 +0100

    update test app with new injectConfigurator params and minor layout change

--- dev/react-test/src/App.jsx
+++ dev/react-test/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { injectConfigurator } from 'ov25-ui';
+import sofaImage from './images/sofa.png';
 
 // Set up a global state for the configurator
 let configuratorInitialized = false;
@@ -11,40 +12,45 @@ const initializeConfigurator = () => {
   // Inject the configurator
   injectConfigurator({
     apiKey: () => { return '15-5f9c5d4197f8b45ee615ac2476e8354a160f384f01c72cd7f2638f41e164c21d' }, 
-    productLink: () => { return '576' }, 
+    // productLink: () => { return '607' }, 
+    // productLink: () => { return 'range/77' },
+    productLink: () => { return 'snap2/4' }, 
+    configureButtonId: { id: '#ov25-fullscreen-button', replace: true },
+    // configurationUuid: () => { return '1ae1bb48-d4d0-41ae-92b4-758a64bdd334' },
     // 217 = Woodbros Pickering-3 Seater (Grouped)
     // range/2 Aughton (Non Grouped)
     // 501 Westbridge/glenwood/grand split option (no filters)
-    onSelectionChange: (selections) => {
-    },
-    onProductChange: (product) => {
-    },
-    images: [
-      'https://demo.orbital.vision/_next/image?url=https%3A%2F%2Flondon-website-general-purpose.s3.eu-west-2.amazonaws.com%2Fassets%2Forganizations%2F15%2Fproducts%2FTamarisk%2F45%2FWindrush-Large-Sofa-Front.png&w=1920&q=75',
-      'https://demo.orbital.vision/_next/image?url=https%3A%2F%2Flondon-website-general-purpose.s3.eu-west-2.amazonaws.com%2Fassets%2Forganizations%2F15%2Fproducts%2FTamarisk%2F45%2FWindrush-Large-Sofa-Angled.png&w=1920&q=75',
-      'https://demo.orbital.vision/_next/image?url=https%3A%2F%2Flondon-website-general-purpose.s3.eu-west-2.amazonaws.com%2Fassets%2Forganizations%2F15%2Fproducts%2FTamarisk%2F45%2FWindrush-Large-Sofa-Side.png&w=1920&q=75',
-      'https://demo.orbital.vision/_next/image?url=https%3A%2F%2Flondon-website-general-purpose.s3.eu-west-2.amazonaws.com%2Fassets%2Forganizations%2F15%2Fproducts%2FTamarisk%2F45%2FWindrush-Large-Sofa-Back.png&w=1920&q=75',
-    ],
-    galleryId: { id: '.configurator-container', replace: true },
+    // onSelectionChange: (selections) => {
+    // },
+    // onProductChange: (product) => {
+    // },
+    // images: [
+    //   'https://demo.orbital.vision/_next/image?url=https%3A%2F%2Flondon-website-general-purpose.s3.eu-west-2.amazonaws.com%2Fassets%2Forganizations%2F15%2Fproducts%2FTamarisk%2F45%2FWindrush-Large-Sofa-Front.png&w=1920&q=75',
+    //   'https://demo.orbital.vision/_next/image?url=https%3A%2F%2Flondon-website-general-purpose.s3.eu-west-2.amazonaws.com%2Fassets%2Forganizations%2F15%2Fproducts%2FTamarisk%2F45%2FWindrush-Large-Sofa-Angled.png&w=1920&q=75',
+    //   'https://demo.orbital.vision/_next/image?url=https%3A%2F%2Flondon-website-general-purpose.s3.eu-west-2.amazonaws.com%2Fassets%2Forganizations%2F15%2Fproducts%2FTamarisk%2F45%2FWindrush-Large-Sofa-Side.png&w=1920&q=75',
+    //   'https://demo.orbital.vision/_next/image?url=https%3A%2F%2Flondon-website-general-purpose.s3.eu-west-2.amazonaws.com%2Fassets%2Forganizations%2F15%2Fproducts%2FTamarisk%2F45%2FWindrush-Large-Sofa-Back.png&w=1920&q=75',
+    // ],
+    // galleryId: { id: '.configurator-container', replace: true },
+    // variantsId: '#ov25-controls',
     priceId: { id: '#price', replace: true },
     nameId: { id: '#name', replace: true },
-    deferThreeD: false,
-    variantsId: '#ov25-controls',
-    swatchesId: '#ov25-swatches',
-    carouselId: true,
-    showOptional: true,
+    // deferThreeD: false,
+    // variantsId: '#ov25-controls',
+    // swatchesId: '#ov25-swatches',
+    // carouselId: true,
+    // showOptional: true,
     logoURL: 'https://ov25.orbital.vision/OV.png',
-
-    addToBasketFunction: () => {
-      alert('Checkout function called');
-    },
-    buyNowFunction: () => {
-      alert('Buy now function called');
-    },
-    addSwatchesToCartFunction: (swatches, swatchRulesData) => {
-      console.log('Add swatches to cart function called with:', { swatches, swatchRulesData });
-      alert(`Add swatches to cart function called with ${swatches.length} swatches`);
-    },
+    // addToBasketFunction: () => {
+    //   alert('Checkout function called');
+    // },
+    // buyNowFunction: () => {
+    //   alert('Buy now function called');
+    // },
+    // addSwatchesToCartFunction: (swatches, swatchRulesData) => {
+    //   console.log('Add swatches to cart function called with:', { swatches, swatchRulesData });
+    //   alert(`Add swatches to cart function called with ${swatches.length} swatches`);
+    // },
+    hidePricing: true,
   });
 
   configuratorInitialized = true;
@@ -78,6 +84,8 @@ function App() {
         break;
       case "CURRENT_SKU":
         break;
+      case "CURRENT_QUERY_STRING":
+        break;
     }
   };
 
@@ -89,19 +97,20 @@ function App() {
 
   return (
     <div className="app ">
-      <h1>OV25 Configurator Test</h1>
+      <h1>Your Website</h1>
       <div className="ov:flex ov:flex-col ov:md:flex-row">
-        <div className="configurator-container ov:w-full ov:aspect-square ov:md:w-[700px] ov:md:h-[500px]"></div>
+        <div className="configurator-container ov:w-full ov:aspect-square ov:md:w-[700px] ov:md:h-[500px]">
+          <img src={sofaImage} alt="Product Image" />
+        </div>
         <div id="ov25-aside-menu" className="ov:w-full ov:md:w-[35%] ov:h-full ov:md:h-[600px] ov:md:mt-0 ov:md:ml-4">
           <div id="price-name" className="ov:w-full ov:md:w-[378px] ov:h-[96px]">
             <div id="price">PRICE: £123</div>
             <div id="name">NAME: Product Name</div>
           </div>
-          <div id="ov25-snap2-fullscreen-button"></div>
-          <div id='ov25-controls' ></div>
+          <div id="ov25-fullscreen-button"></div>
+          <div id='ov25-controls'></div>
           <div id="ov25-swatches"></div>
-          <p className='ov:mt-4'>This is a description of the product. Words will go here - it might continue for a few lines. It might be a few sentences long. </p>
-        </div>
+           </div>
       </div>
     </div>
   );
