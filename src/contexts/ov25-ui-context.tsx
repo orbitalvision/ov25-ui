a8e260b7ae88967f11e226fddea06c4459e5639a
tom williams <tom.williams@orbital.vision>
2025-10-17 09:09:56 +0100

    fix switching to modules tab when module is selected

--- src/contexts/ov25-ui-context.tsx
+++ src/contexts/ov25-ui-context.tsx
@@ -882,7 +882,7 @@ export const OV25UIProvider: React.FC<{
             break;
           case 'COMPATIBLE_MODULES':
             setCompatibleModules(data.modules || []);
-            if (data.modules.length > 0 && (configuratorState?.snap2Objects?.length || hasConfigureButton) && !preloading && !isMobile) {
+            if (data.modules.length > 0 && !preloading && !isMobile) {
               setIsVariantsOpen(true);
               setActiveOptionId('modules');
             }
