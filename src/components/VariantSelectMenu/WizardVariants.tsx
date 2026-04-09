66e0748087a9156f73ef95b5c1356411beb3249d
tom williams <tom.williams@orbital.vision>
2026-04-09 13:16:55 +0100

    stop swatch icons showing above filter content on wizard.

--- src/components/VariantSelectMenu/WizardVariants.tsx
+++ src/components/VariantSelectMenu/WizardVariants.tsx
@@ -375,7 +375,7 @@ export const WizardVariants: React.FC<WizardVariantsProps> = ({ mode }) => {
             {isFilterOpenForOption && currentOption && (
               <div
                 data-open={isFilterOpenForOption}
-                className="ov:flex ov:justify-end ov:flex-wrap ov:overflow-y-auto ov:absolute ov:inset-0 ov:h-full ov:p-2 ov:px-4 ov:bg-[var(--ov25-background-color)] ov:transition-transform ov:duration-500 ov:ease-in-out ov:translate-y-0"
+                className="ov:flex ov:justify-end ov:flex-wrap ov:overflow-y-auto ov:z-8 ov:absolute ov:inset-0 ov:h-full ov:p-2 ov:px-4 ov:bg-[var(--ov25-background-color)] ov:transition-transform ov:duration-500 ov:ease-in-out ov:translate-y-0"
               >
                 <FilterContent optionId={currentOption.id} />
               </div>
