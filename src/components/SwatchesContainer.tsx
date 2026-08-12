14c9a0ed7d057819c33f1ace2ecb63cc6c42b7a6
tom williams <tom.williams@orbital.vision>
2026-08-12 10:27:47 +0100

    fix(swatchbook) fall back to swatch placeholder image

--- src/components/SwatchesContainer.tsx
+++ src/components/SwatchesContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useOV25UI } from '../contexts/ov25-ui-context.js';
 import { SwatchImage } from './SwatchImage.js';
+import { PLACEHOLDER_IMAGE_URL } from '../lib/placeholder-image.js';
 
 export const SwatchesContainer: React.FC = () => {
   const { selectedSwatches, swatchRulesData, setIsSwatchBookOpen, hasSelectionsWithSwatches, getString } = useOV25UI();
@@ -17,7 +18,7 @@ export const SwatchesContainer: React.FC = () => {
     <div id="ov25-selected-swatches-container" className="ov:flex ov:flex-row ov:items-center ov:max-w-[300px] ov:bg-(--ov25-background-color) ov:mt-2 ov:gap-2 ov:border ov:border-(--ov25-border-color) ov:rounded-full ov:py-2 ov:px-4 ov:cursor-pointer" onClick={toggleSwatchBook}>
       {(selectedSwatches.length > 0 && selectedSwatches[0].thumbnail && selectedSwatches[0].thumbnail.miniThumbnails) ? (
         <SwatchImage
-          src={selectedSwatches[0].thumbnail.miniThumbnails.small}
+          src={selectedSwatches[0].thumbnail.miniThumbnails.small || PLACEHOLDER_IMAGE_URL}
           alt={selectedSwatches[0].name}
           className="ov:w-10 ov:h-10 ov:max-w-20 ov:md:max-w-24 ov:aspect-square"
         />
@@ -62,4 +63,4 @@ export const SwatchesContainer: React.FC = () => {
       </div>
     </div>
   );
-}; 
+};
