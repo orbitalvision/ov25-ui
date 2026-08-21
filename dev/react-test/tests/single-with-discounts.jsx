509bdf23087095aa0615daecce07af0fc5ac05f4
tom williams <tom.williams@orbital.vision>
2026-08-21 14:05:35 +0100

    (tests) update test to DEV_RETAILER

--- dev/react-test/tests/single-with-discounts.jsx
+++ dev/react-test/tests/single-with-discounts.jsx
@@ -3,20 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { TestPageLayout } from '../templates/TestPageLayout.jsx';
 import '../src/index.css';
 
-const DEMO_RETAILER_APIKEY = import.meta.env.VITE_DEMO_RETAILER_APIKEY;
+const DEV_RETAILER_APIKEY = import.meta.env.VITE_DEV_RETAILER_APIKEY;
 
 const config = /** @type {import('ov25-ui').InjectConfiguratorInput} */ ({
-  apiKey: () => DEMO_RETAILER_APIKEY,
-  productLink: () => '1313',
+  apiKey: () => DEV_RETAILER_APIKEY,
+  productLink: () => '1682',
   selectors: {
     gallery: { selector: '.configurator-container', replace: true },
     variants: '#ov25-controls',
     price: { selector: '#price', replace: true },
-    name: { selector: '#name', replace: true },
   },
   carousel: { desktop: 'stacked', mobile: 'carousel' },
   configurator: {
-    displayMode: { desktop: 'sheet', mobile: 'drawer' },
+    displayMode: { desktop: 'inline', mobile: 'drawer' },
     triggerStyle: { desktop: 'single-button', mobile: 'single-button' },
     variants: { displayMode: { desktop: 'tree', mobile: 'list' } },
   },
@@ -32,7 +31,7 @@ function App() {
   return (
     <TestPageLayout
       title="Single Product - With Discounts"
-      description="Westbury Large"
+      description="Pickering 3 Seater"
       injectConfig={config}
     />
   );
